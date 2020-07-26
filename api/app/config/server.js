b740cb6cba06603c9f641c2cc77f1d7d6cf5af56
azhariemuhammad <azharie_muhammad@live.com>
2020-07-27 00:11:10 +0700

    add api

---
+++ api/app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5a9d8a521fdd44241186cc3ea02774ec'),
+    },
+  },
+});
---
+++ api/app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8958af3c-ed1e-4511-9be4-cfd0dee219fd'
+};
