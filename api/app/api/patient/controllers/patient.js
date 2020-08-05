44c57b7af0f230f7337b0526ed8994906f82741e
azhariemuhammad <azharie_muhammad@live.com>
2020-08-05 09:10:06 +0700

    add search patients service

--- api/app/api/patient/controllers/patient.js
+++ api/app/api/patient/controllers/patient.js
@@ -1,8 +1,15 @@
-'use strict';
-
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  find: async ctx => {
+    console.log(ctx.query)
+
+    if (ctx.query) {
+      return strapi.services.patient.search(ctx.query)
+    }
+    return 'fooo'
+  }
+}
--- api/app/api/patient/services/patient.js
+++ api/app/api/patient/services/patient.js
@@ -1,8 +1,21 @@
-'use strict';
-
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/services.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  search: async params => {
+    const { ktp_id, mr_code, first_name } = params
+    // return strapi
+    //   .query('patient')
+    //   .find({ ktp_id, first_name })
+    //   .populate(['first_name', 'last_name'])
+    const result = await strapi
+      .query('medical-record')
+      .model.query(qb => {
+        qb.where('mr_code', mr_code)
+      })
+      .fetch()
+    return result.toJSON()
+  }
+}
