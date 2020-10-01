fb92d8bdcb249bc7c3fa7d9975215af3509dfb5b
azhariemuhammad <azharie_muhammad@live.com>
2020-10-01 22:06:13 +0800

    create multiple row diagnose

--- api/app/api/diagnose/controllers/diagnose.js
+++ api/app/api/diagnose/controllers/diagnose.js
@@ -1,8 +1,16 @@
-'use strict';
-
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
-
-module.exports = {};
+module.exports = {
+  add: async ctx => {
+    const { body } = ctx.request
+    let entity
+    if (Array.isArray(body)) {
+      entity = await Promise.all(
+        body.map(val => {
+          return strapi.services.diagnose.create(val)
+        })
+      )
+    } else {
+      entity = await strapi.services.diagnose.create(body)
+    }
+    return entity
+  }
+}
--- api/app/api/diagnose/services/diagnose.js
+++ api/app/api/diagnose/services/diagnose.js
@@ -1,8 +1,5 @@
-'use strict';
-
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/services.html#core-services)
- * to customize this service
- */
-
-module.exports = {};
+module.exports = {
+  create: async value => {
+    return strapi.query('diagnose').create(value)
+  }
+}
